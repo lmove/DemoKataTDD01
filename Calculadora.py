4fa5b5fd1e973a7129319929ee1ef07da64add6a
Lina <lina.m8a@gmail.com>
2016-03-15 11:12:28 -0500

    Documentación y refactoring.

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Clase encargada de efectuar una suma de números
+# a partir de una cadena.
 class Calculadora:
 
+    # Función encargada de hacer la suma de un listado
+    # de números que entran por parámetro, separados por
+    # ",", "&" o ":"
     def sumar(self, cadena):
         if len(cadena) == 0:
             return 0
@@ -16,3 +24,6 @@ class Calculadora:
                     cont += int(a)
 
                 return cont
+
+            else:
+                raise ValueError("El formato de la cadena es incorrecto.")
--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -6,17 +6,22 @@ from Calculadora import Calculadora
 
 class CalculadoraTest(TestCase):
 
+    # Prueba encargada de validar el retorno de 0 [ara una cadena vacía.
     def test_sumar_cadena_vacia(self):
         self.assertEqual(Calculadora().sumar(""), 0, "Sumar cadena vacía")
 
+    # Prueba encargada de validar la suma de un solo número.
     def test_sumar_cadena_un_numero(self):
         self.assertEqual(Calculadora().sumar("1"), 1, "Sumar cadena con un número")
 
+    # Prueba encargada de validar la suma de dos números separados por ",".
     def test_sumar_cadena_dos_numeros(self):
         self.assertEqual(Calculadora().sumar("1,2"), 3, "Sumar cadena con dos números")
 
+    # Prueba encargada de validar la suma de varios números separados por ",".
     def test_sumar_cadena_varios_numeros(self):
         self.assertEqual(Calculadora().sumar("1,2,5,7,3"), 18, "Sumar cadena con varios números")
 
+    # Prueba encargada de validar la suma de varios números separados por ",", "&" y/o ":".
     def test_sumar_cadena_varios_numeros_separadores(self):
         self.assertEqual(Calculadora().sumar("1&3:4,8&2:6,5"), 29, "Sumar cadena con varios números y separadores")
