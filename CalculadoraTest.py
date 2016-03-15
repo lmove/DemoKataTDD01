6146b4112190fd215fca8f9cf88d6825f5bd2a2f
Lina <lina.m8a@gmail.com>
2016-03-15 11:02:51 -0500

    Prueba 5 - Sumar cadena con varios números y separadores (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -17,3 +17,6 @@ class CalculadoraTest(TestCase):
 
     def test_sumar_cadena_varios_numeros(self):
         self.assertEqual(Calculadora().sumar("1,2,5,7,3"), 18, "Sumar cadena con varios números")
+
+    def test_sumar_cadena_varios_numeros_separadores(self):
+        self.assertEqual(Calculadora().sumar("1&3:4,8&2:6,5"), 28, "Sumar cadena con varios números y separadores")
