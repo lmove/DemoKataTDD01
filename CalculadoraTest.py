efa8ab348960cd89d4d5bd1b74ba4ef4f2909ac8
Lina <lina.m8a@gmail.com>
2016-03-15 10:50:49 -0500

    Prueba 2 - Sumar cadena con un número (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -5,5 +5,9 @@ from unittest import TestCase
 from Calculadora import Calculadora
 
 class CalculadoraTest(TestCase):
-    def test_sumar(self):
+
+    def test_sumar_cadena_vacia(self):
         self.assertEqual(Calculadora().sumar(""), 0, "Sumar cadena vacía")
+
+    def test_sumar_cadena_un_numero(self):
+        self.assertEqual(Calculadora().sumar("1"), 1, "Sumar cadena con un número")
